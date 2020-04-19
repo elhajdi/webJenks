3ff1b7b2772592f0e7d5102d66342e47ec04c457
elhajdi <mohamed.elhajdi@gmail.com>
2020-04-19 15:23:31 +0000

    full-build phpci docuemntation

--- src/Email.php
+++ src/Email.php
@@ -1,5 +1,7 @@
 <?php declare(strict_types=1);
-
+/**
+ *  emailing class
+ */
 final class Email
 {
     private $email;
@@ -11,16 +13,30 @@ final class Email
         $this->email = $email;
     }
 
+    /**
+     * [fromString description]
+     * @param  string $email [description]
+     * @return [type]        [description]
+     */
     public static function fromString(string $email): self
     {
         return new self($email);
     }
 
+    /**
+     * [__toString description]
+     * @return string [description]
+     */
     public function __toString(): string
     {
         return $this->email;
     }
 
+    /**
+     * [ensureIsValidEmail description]
+     * @param  string $email [description]
+     * @return [type]        [description]
+     */
     private function ensureIsValidEmail(string $email): void
     {
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
